11e85d84aca1076c9ee79bb0f2647e2ce5368e53
Emily Loiselle <emilyannaloiselle@gmail.com>
2024-10-30 17:05:00 -0400

    SCRUM-43: Fixed bug in PackageUpload and cleaned up logging to use Logger, added env variable to optionaly print logging to console

--- backend/src/services/package/PackageUploadService.ts
+++ backend/src/services/package/PackageUploadService.ts
@@ -4,51 +4,64 @@ import fs from 'fs';
 import { PackageMetadata } from '../../models/package/PackageMetadata';
 import { PackageData } from '../../models/package/PackageData';
 import { S3 } from '../../utils/S3';
+import { Logger } from '../../utils/Logger';
 
 export class PackageUploadService {
     public async uploadPackage() {
     }
 
     public static extractPackageInfo(packageData: PackageData) : PackageMetadata {
+        Logger.logInfo("Extracting package metadata");
         const zipBuffer = Buffer.from(packageData.getContent(), 'base64');
         const zip = new AdmZip(zipBuffer);
         let packageMetadata: PackageMetadata | null = null;
         let readmeContent: string = '';
+        let readmePathParts = 0; // Used to keep track of readme file path
 
+        Logger.logInfo("Looping through files in repository")
         zip.getEntries().forEach((entry) => {
             const fileName = entry.entryName;
-            console.log(fileName);
-            if (fileName.endsWith('package.json')) {
+
+            const pathParts = fileName.split('/');
+            if (fileName.endsWith('package.json') && pathParts.length === 2) {
                 const packageJsonContent = entry.getData().toString('utf8');
                 const packageInfo = JSON.parse(packageJsonContent);
 
                 packageMetadata = new PackageMetadata(packageInfo.name, packageInfo.version);
+                if(!packageInfo.repository?.url) {
+                    throw new Error("400: No url found in the uploaded package's package.json");
+                }
                 packageMetadata.setUrl(packageInfo.repository?.url);
             }
 
             // TODO: What to do if no readme is found?
             if (fileName.toLowerCase().endsWith('readme.md')) {
-                readmeContent = entry.getData().toString('utf8');
+                if (readmePathParts != 0) {
+                    if (pathParts.length < readmePathParts) {
+                        readmeContent = entry.getData().toString('utf8');
+                        readmePathParts = pathParts.length;
+                    }
+                } else {
+                    readmeContent = entry.getData().toString('utf8');
+                    readmePathParts = pathParts.length;
+                }
             }
         });
 
         if (packageMetadata === null) {
+            Logger.logInfo("No package.json found in the uploaded package");
             throw new Error('400: No package.json found in the uploaded package');
         }
-
-        if (packageMetadata && readmeContent) {
-            (packageMetadata as PackageMetadata).setReadMe(readmeContent);
+        else {
+            // Set readme content if found
+            if (readmeContent) {
+                (packageMetadata as PackageMetadata).setReadMe(readmeContent);
+            }
+            else {
+                Logger.logInfo("No README.md found in the uploaded package");
+                throw new Error('400: No README.md found in the uploaded package');
+            }
         }
         return packageMetadata;
-    }
-
-    public static async uploadToS3(packageData: PackageData, packageId: string) {
-        try {
-            const buffer = Buffer.from(packageData.getContent(), 'base64');
-
-
-        } catch (error) {
-            console.error('Error uploading package to S3:', error);
-        }
     }
 }
--- backend/tunnel.sh
+++ backend/tunnel.sh
@@ -11,8 +11,28 @@ EC2_USER="ubuntu"
 EC2_IP="3.129.240.110"
 
 
+# Check that key path has been set
+if [ -z "$SSH_KEY_PATH_" ]; then
+    echo "SSH_KEY_PATH_ is not set. Please set it in set_env.sh"
+    exit 1
+fi
+
 # Path to your SSH key file (if needed)
 SSH_KEY_PATH="$SSH_KEY_PATH_"
 
-# Create the SSH tunnel in the background
-ssh -i $SSH_KEY_PATH -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $EC2_USER@$EC2_IP -N & 
+
+if [ "$1" == "--close" ]; then
+    # Find and kill the process listening on port 5432
+    PID=$(netstat -ano | grep ':5432' | grep LISTEN | awk '{print $5}' | head -n 1)
+
+    if [ -n "$PID" ]; then
+    taskkill //PID "$PID" //F
+    echo "Process on port 5432 has been terminated."
+    else
+    echo "No process is listening on port 5432."
+    fi
+else
+    # Create the SSH tunnel in the background
+    echo "Creating SSH tunnel to $EC2_IP"
+    ssh -i $SSH_KEY_PATH -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $EC2_USER@$EC2_IP -N &
+fi
