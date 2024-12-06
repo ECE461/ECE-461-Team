77a3e9d7fcb6911c08515c1a11e72722e6d4b760
Emily Loiselle <emilyannaloiselle@gmail.com>
2024-12-06 01:19:24 -0500

    Added frontend setup to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import { PackageEndpoints } from './endpoints/PackageEndpoints';
 import { Logger } from './utils/Logger';
 import { exec } from 'child_process';
+import path from 'path';
 
 const cors = require('cors');
+const next = require('next');
 
 // Check that all required env variables have been set:
 if (!process.env.RDS_USER || !process.env.RDS_KEY || !process.env.RDS_HOST || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.GITHUB_TOKEN || !process.env.SSH_KEY_PATH_ || !process.env.JWT_KEY) {
@@ -31,17 +33,27 @@ if (!process.env.RDS_USER || !process.env.RDS_KEY || !process.env.RDS_HOST || !p
     (async () => {
         try {
             const app = express();
-            const port = process.env.PORT || 3000;
-            const baseURL = '/api/v1';
+            const apiURL = '/api';
             app.use(cors());
             app.use(express.json({limit: '10mb'}));
             
-            
+            // API Routes:
             const packageEndpoints = new PackageEndpoints();
-            app.use(baseURL, packageEndpoints.getRouter());
+            app.use(apiURL, packageEndpoints.getRouter());
+
+            const nextApp = next({ dev: false, dir: path.join(__dirname, '../../frontend') });
+            const handle = nextApp.getRequestHandler();
+            await nextApp.prepare();
+            app.all('*', (req, res) => handle(req, res));
             
-            app.listen(port, () => {
-                console.log(`Server running at http://localhost:${port}${baseURL}`);
+            // Set HOST to 0.0.0.0 in EC2
+            const host = process.env.HOST ? process.env.HOST : 'localhost';
+            const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+            app.listen(port, host, () => {
+                console.log(`Backend server running at http://localhost:${port}${apiURL}`);
+                console.log(`Frontend server running at http://localhost:${port}`);
+                Logger.logInfo(`Backend server running at http://localhost:${port}${apiURL}`);
+                Logger.logInfo(`Frontend server running at http://localhost:${port}`);
             });
         } catch (error) {
             console.error('Error starting the application:', error);
