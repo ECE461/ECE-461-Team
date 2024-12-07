686ab6ba9f1a082b7e09eb794b05f4466a931073
Emily Loiselle <emilyannaloiselle@gmail.com>
2024-12-07 02:38:52 -0500

    Update to match autograder

--- backend/src/controllers/PackageQueryController.ts
+++ backend/src/controllers/PackageQueryController.ts
@@ -315,6 +315,7 @@ export class PackageQueryController {
     static async logRequest(req: Request, endpoint: string) {
         Logger.logInfo(`*******************REQUEST*******************`);
         Logger.logInfo(`            ${endpoint}`);
+        Logger.logDebug(`Request headers: ${JSON.stringify(req.headers)}`);
         Logger.logDebug(`Request Body: ${JSON.stringify(req.body)}`);
         Logger.logDebug (`Request Params: ${JSON.stringify(req.params)}`);
         Logger.logDebug(`Request query: ${JSON.stringify(req.query)}`);
