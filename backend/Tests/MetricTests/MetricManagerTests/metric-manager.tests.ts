712b7b802d80beb776908654efe5ba878e3db18f
henson14 <henson14@purdue.edu>
2024-12-07 14:36:43 -0500

    finished metric manager testing

--- backend/Tests/MetricTests/MetricManagerTests/metric-manager.tests.ts
+++ backend/Tests/MetricTests/MetricManagerTests/metric-manager.tests.ts
@@ -60,70 +60,28 @@ describe('Metric Manager Tests', () => {
       expect(metrics.netScore).toBe(0);
     });
 
+    it ('should return 0.5 when all subscores are 0.5', async () => {
+      // have to mock dependency getJson as well
+      jest.spyOn(dependency, 'getPackageJson').mockResolvedValue({
+        "name": "my-package",
+        "version": "1.0.0",
+        "dependencies": {
+          "express": "^4.17.1",
+          "lodash": "^4.17.21"
+        }
+      });
 
-    // test('Get Metrics for different valid repository', async () => {
-    //     const manager = new MetricManager('lodash/lodash');
-    //     const metrics = await manager.getMetrics();
-    //     expect(metrics).toEqual({
-    //         netScore: 0.758,
-    //         netLatency: expect.any(Number),
-    //         rampUpValue: 0.992,
-    //         rampUpLatency: expect.any(Number),
-    //         correctnessValue: 0.85,
-    //         correctnessLatency: expect.any(Number),
-    //         busFactorValue: 0.5,
-    //         busFactorLatency: expect.any(Number),
-    //         maintainerValue: 0.8,
-    //         maintainerLatency: expect.any(Number),
-    //         licenseValue: 1,
-    //         licenseLatency: expect.any(Number)
-    //     });
-    // });
+      jest.spyOn(BusFactor.prototype, 'calculateBusFactor').mockResolvedValue(0.5);
+      jest.spyOn(Correctness.prototype, 'getCorrectnessScore').mockResolvedValue(0.5);
+      jest.spyOn(dependency.DependencyMetric.prototype, 'getPackageScore').mockReturnValue(0.5);
+      jest.spyOn(License.prototype, 'getRepoLicense').mockResolvedValue(1); // License is only 1 or 0
+      jest.spyOn(Maintainer.prototype, 'getMaintainerScore').mockResolvedValue(0.5);
+      jest.spyOn(PullRequest.prototype, 'getPullRequest').mockResolvedValue(0.5);
+      jest.spyOn(RampUp.prototype, 'getRampUpScore').mockResolvedValue(0.5);
 
-
-    // test('Get Metrics for invalid repository path', async () => {
-    //     expect(() => new MetricManager('invalidpath')).toThrow('Invalid GitHub repository URL');
-    // });
-
-    // test('Get Metrics for empty repository path', async () => {
-    //     expect(() => new MetricManager('')).toThrow('Invalid GitHub repository URL');
-    // });
-
-    // test('get owner and repo name', async () => {
-    //     const manager = new MetricManager('cloudinary/cloudinary_npm');
-    //     const owner = manager.getOwner();
-    //     const repo = manager.getRepoName();
-    //     expect(owner).toBe('cloudinary');
-    //     expect(repo).toBe('cloudinary_npm');
-    // });
-    // /* test('Get Metrics', async () => {
-    //     const manager = new MetricManager('cloudinary/cloudinary_npm');
-    //     const metrics = await manager.getMetrics();
-    //     expect(metrics).toBe('');
-    // });
-
-    // test('Get different metrics', async () => {
-    //     const manager = new MetricManager('lodash/lodash');
-    //     const metrics = await manager.getMetrics();
-    //     expect(metrics).toBe('Metrics: [ '+
-    //     '\n URL:' +
-    //     'busFactorValue: 0.5 (Latency: 0.255 s)' +
-    //     'rampUpValue: 0.992 (Latency: 0.362 s)' +
-    //     'licenseValue: 1 (Latency: 0.426 s)' +
-    //     'maintainerValue: 0.8 (Latency: 0.619 s)' +
-    //     'correctnessValue: 0.85 (Latency: 1.056 s)' +
-    //     'Net Score: 0.758 (Latency: 2.719 s)' +
-    //     ' ] for lodash / lodash');
-    // });
-
-    // test('Get empty metrics', async () => {
-    //     try {
-    //         const manager = new MetricManager('');
-    //         await manager.getMetrics();
-    //     } catch (error) {
-    //         expect(error).toBeInstanceOf(Error);
-    //     }
-
-    // }); */
+      const manager = new MetricManager('cloudinary/cloudinary_npm');
+      const metrics = await manager.getMetrics();
+      expect(metrics.netScore).toBe(0.5);
+    });
 
 });
